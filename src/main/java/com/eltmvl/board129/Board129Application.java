17ecbe74f01cc9ee71163eeaae750cb231456f01
eltmvl <eltmvl@naver.com>
2023-01-29 18:40:08 +0900

    BaseTimeEntity-register-is-ok

--- src/main/java/com/eltmvl/board129/Board129Application.java
+++ src/main/java/com/eltmvl/board129/Board129Application.java
@@ -2,7 +2,9 @@ package com.eltmvl.board129;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Board129Application {
 
