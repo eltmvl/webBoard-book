6f92d57c15870a51e9cf98656c323d25d2e27699
eltmvl <eltmvl@naver.com>
2023-01-29 20:17:36 +0900

    generate-List

--- src/main/java/com/eltmvl/board129/service/posts/PostsService.java
+++ src/main/java/com/eltmvl/board129/service/posts/PostsService.java
@@ -3,12 +3,16 @@ package com.eltmvl.board129.service.posts;
 
 import com.eltmvl.board129.domain.posts.Posts;
 import com.eltmvl.board129.domain.posts.PostsRepository;
+import com.eltmvl.board129.web.Dto.PostsListResponseDto;
 import com.eltmvl.board129.web.Dto.PostsResponseDto;
 import com.eltmvl.board129.web.Dto.PostsSaveRequestDto;
 import com.eltmvl.board129.web.Dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +33,10 @@ public class PostsService {
     public PostsResponseDto findById (Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
