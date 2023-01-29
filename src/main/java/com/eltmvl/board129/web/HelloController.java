6de263fa01efb623d2ac490aaa50bf6850f446cf
eltmvl <eltmvl@naver.com>
2023-01-29 13:46:48 +0900

    TDD-UNIT-TEST

--- src/main/java/com/eltmvl/board129/web/HelloController.java
+++ src/main/java/com/eltmvl/board129/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.eltmvl.board129.web;
 
+import com.eltmvl.board129.web.Dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -11,4 +13,11 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+
 }
--- src/test/java/com/eltmvl/board129/web/HelloControllerTest.java
+++ src/test/java/com/eltmvl/board129/web/HelloControllerTest.java
@@ -2,19 +2,16 @@ package com.eltmvl.board129.web;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 /*
@@ -40,6 +37,17 @@ public class HelloControllerTest {
                    OK = 200
                 */
                 .andExpect(content().string(hello));
+    }
+    @Test
+    public void helloDto_return() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto").param("name", name)
+                                                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 
 }
