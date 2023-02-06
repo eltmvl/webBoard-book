2aaf490fdea353e1bab8c3a3bdad2f6426cad950
eltmvl <eltmvl@naver.com>
2023-02-06 22:01:15 +0900

    plzplzplzplz

--- src/test/java/com/eltmvl/board129/Board129ApplicationTests.java
+++ src/test/java/com/eltmvl/board129/Board129ApplicationTests.java
@@ -3,7 +3,7 @@ package com.eltmvl.board129;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Board129ApplicationTests {
 
     @Test
